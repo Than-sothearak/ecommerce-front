bc8697f9ca8ecc828301a7a2cdf5ab2d090f0f0f
Than-sothearak <thearak643@gmail.com>
2023-09-18 02:49:47 +0700

    fixed reviews

---
+++ .history/components/NewProduct_20230918011751.js
@@ -0,0 +1,26 @@
+import { styled } from "styled-components";
+import ProductBox from "./ProductBox";
+import Center from "./Center";
+import { useState, useEffect } from "react";
+import ProductGrid from "./ProductGrid";
+
+
+const Title = styled.h2`
+font-size: 2rem;
+margin: 30px 0 10px;
+font-weight: normal;`
+
+
+const NewProduct = ({ newProduct, wishedProduct, reviews }) => {
+  
+  return (
+    <Center>
+      <Title>New arrivals</Title>
+      <ProductGrid 
+      products={newProduct} 
+      wishedProduct={wishedProduct}/>
+    </Center>
+  );
+};
+
+export default NewProduct;
---
+++ .history/components/ProductGrid_20230918013017.js
@@ -0,0 +1,39 @@
+import { styled } from "styled-components";
+import ProductBox from "./ProductBox";
+import { RevealWrapper } from "next-reveal";
+import PcBox from "./PcBox";
+
+const StyledProductGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 20px;
+
+  transition: visibility 1s linear;
+ 
+  @media screen and (min-width: 740px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+
+  @media screen and (min-width: 1280px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+`;
+export default function ProductGrid({ products, wishedProduct = [], reviews }) {
+  return (
+    <StyledProductGrid>
+      {products?.length > 0 &&
+        products.map((product, index) => (
+          <RevealWrapper
+            key={product._id}
+            delay={50*index}
+            duration={1000}
+          >
+            <PcBox {...product}
+            reviews={reviews.filter(p => (product._id))}
+            wished={wishedProduct.includes(product._id)}/>
+          </RevealWrapper>
+        ))}
+    </StyledProductGrid>
+  );
+}
